9717ce986379dd76b7dae9650a62049232e139d7
AGYTgit <bubonb12@gmail.com>
2024-10-04 20:02:25 +0200

    mouse tracking

--- Game1.cs
+++ Game1.cs
@@ -11,7 +11,11 @@ public class Asteroids : Game
     Vector2 ss_position;
     float ss_rotation;
     float ss_speed_forward;
+    float ss_speed_forward_multiplier;
     float ss_speed_other;
+    float ss_speed_other_multiplier;
+    float ss_rotate_speed;
+    float ss_rotate_speed_multiplier;
 
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
@@ -20,7 +24,8 @@ public class Asteroids : Game
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
-        IsMouseVisible = false;
+        IsMouseVisible = true;
+        IsFixedTimeStep = false;
     }
 
     protected override void Initialize()
@@ -31,7 +36,11 @@ public class Asteroids : Game
                                     _graphics.PreferredBackBufferHeight / 2);
 
         ss_speed_forward = 100f;
+        ss_speed_forward_multiplier = 1f;
         ss_speed_other = 50f;
+        ss_speed_other_multiplier = 1f;
+        ss_rotate_speed = .02f;
+        ss_rotate_speed_multiplier = 1f;
 
         base.Initialize();
     }
@@ -51,10 +60,8 @@ public class Asteroids : Game
             Exit();
         }
 
-        // TODO: Add your update logic here
-
-        float updated_ss_speed_forward = ss_speed_forward * (float)gameTime.ElapsedGameTime.TotalSeconds;
-        float updated_ss_speed_other = ss_speed_other * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float updated_ss_speed_forward = ss_speed_forward * ss_speed_forward_multiplier * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float updated_ss_speed_other = ss_speed_other * ss_speed_other_multiplier * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
         var kstate = Keyboard.GetState();
         
@@ -76,17 +83,52 @@ public class Asteroids : Game
             ss_position.X += updated_ss_speed_other * (float)Math.Cos(ss_rotation);
         }
 
-        if (kstate.IsKeyDown(Keys.W)) {
-            ss_rotation -= .05f;
-        }
-
         if (kstate.IsKeyDown(Keys.R)) {
-            ss_rotation += .05f;
-        }
-
-        if (kstate.IsKeyDown(Keys.Space)) {
             ss_position = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2) - new Vector2(ss_sprite.Width / 2, ss_sprite.Height / 2);
         }
+
+        if (kstate.IsKeyDown(Keys.Space)){
+            ss_speed_forward_multiplier = 8f;
+            ss_speed_other_multiplier = 4f;
+            ss_rotate_speed_multiplier = 4f;
+        } else {
+            ss_speed_forward_multiplier = 1f;
+            ss_speed_other_multiplier = 1f;
+            ss_rotate_speed_multiplier = 1f;
+        }
+
+
+
+        Vector2 mouse_position = new Vector2(Mouse.GetState().Position.X, Mouse.GetState().Position.Y);
+
+        Vector2 direction = mouse_position - ss_position;
+        float target_angle_r = MathF.Atan2(direction.Y, direction.X);
+
+        ss_rotation = (ss_rotation + (float)(Math.PI * 2)) % (float)(Math.PI * 2);
+
+        float angle_diff = target_angle_r + (float)(Math.PI / 2) - ss_rotation;
+
+
+        if (angle_diff > MathF.PI)
+        {
+            angle_diff -= 2 * MathF.PI;
+        }
+        else if (angle_diff < -MathF.PI)
+        {
+            angle_diff += 2 * MathF.PI;
+        }
+
+
+        if (angle_diff > 0) {
+            ss_rotation += ss_rotate_speed * ss_rotate_speed_multiplier;
+        } else if (angle_diff < 0) {
+            ss_rotation -= ss_rotate_speed * ss_rotate_speed_multiplier;
+        }
+
+        
+
+        // ss_rotation = angle_r + (float)(Math.PI / 2);
+
 
         base.Update(gameTime);
     }
--- obj/Debug/net8.0/Asteroids.AssemblyInfo.cs
+++ obj/Debug/net8.0/Asteroids.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asteroids")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16712681a55ff1f3ad9a45d669a05200a4185f79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18896a246961b11b680162183c8e8d43d31d071d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asteroids")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asteroids")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
